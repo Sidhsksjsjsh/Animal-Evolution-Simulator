903ff25ab7b4d5be23f8c380466d91ee3e2c090c
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-01-27 08:42:37 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,6 +1,7 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VAPE-UI-MODDED/main/.lua"))()
 local wndw = lib:Window("VIP Turtle Hub V4 - BETA")
 local T1 = wndw:Tab("Main")
+local client = game.Players.LocalPlayer
 
 T1:Toggle("Auto attack",false,function(value)
    _G.Attack = value
@@ -13,5 +14,7 @@ end)
 lib:HookCalled(function(self,args)
      if self == "UpdateStatEvent" and args[1] == "giftDailyTimeLeft" then
       args[2] = 0
+    elseif self == "SetExpTextEvent" and args[1] == client then
+         args[2] = 9e9
     end
 end)
