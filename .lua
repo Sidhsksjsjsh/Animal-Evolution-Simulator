f167c9ff09f9bd52a0035ac122030eb700107586
Fahri <117529823+Sidhsksjsjsh@users.noreply.github.com>
2024-02-03 09:50:15 +0900

    Update .lua

--- .lua
+++ .lua
@@ -1,15 +1,36 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Sidhsksjsjsh/VAPE-UI-MODDED/main/.lua"))()
-local wndw = lib:Window("VIP Turtle Hub V4 - Cheating Tool")
+local wndw = lib:Window("VIP Turtle Hub V4 - More bypass, more fun")
 local T1 = wndw:Tab("Main")
 local T2 = wndw:Tab("Upgrade")
 local T3 = wndw:Tab("Chest & Item")
 local T4 = wndw:Tab("Animal Changer")
+local T5 = wndw:Tab("Leaderstats")
 
 local workspace = game:GetService("Workspace")
 local client = game.Players.LocalPlayer
 local skinHandler = {}
 
 lib:AddTable(game:GetService("ReplicatedStorage")["AnimalsSkin"],skinHandler)
+--[[
+Players.Rivanda_Cheater.leaderstats.Rebirth
+Players.Rivanda_Cheater.leaderstats.Xp
+]]
+
+local function getGoldRebirth()
+   return client["leaderstats"]["G Rebirth"]["Value"]
+end
+
+local function getRebirth()
+   return client["leaderstats"]["Rebirth"]["Value"]
+end
+
+local function getXP()
+   return client["leaderstats"]["Xp"]["Value"]
+end
+
+local function getLevel()
+   return client["leaderstats"]["Lv"]["Value"]
+end
 
 T1:Toggle("Auto attack",false,function(value)
    _G.Attack = value
@@ -44,10 +65,12 @@ T1:Toggle("Auto gold rebirth",false,function(value)
     end
 end)
 
-T1:Button("Rebirth area",function()
-      workspace:FindFirstChild("Teleport Gui [FE]5")["ScriptTeleportRebirth"]["RemoteFunction"]:InvokeServer()
-      --wait(0.5)
-      --lib:FireTouch(workspace["RebMeat"])
+T1:Toggle("Auto rebirth",false,function(value)
+   _G.rbrts = value
+    while wait() do
+      if _G.rbrts == false then break end
+      game:GetService("ReplicatedStorage")["Events"]["RebirthEvent"]:FireServer()
+    end
 end)
 
 --[[T1:Button("Instant collect apples",function()
@@ -87,11 +110,6 @@ T4:Button("Change animal",function()
    game:GetService("ReplicatedStorage")["Events"]["ChangeRaceEvent"]:FireServer(_G.animaltype)
 end)
 
---[[lib:HookCalled(function(self,args)
-     if self == "UpdateStatEvent" and args[1] == "giftDailyTimeLeft" then
-         args[2] = 0
-    elseif self == "SetExpTextEvent" and args[1] == client then
-         args[2] = 9e9
-    end
-end)
-]]
+while wait() do
+   T5:Label("[ Your leaderstats ]\nLevel: " .. getLevel() .. "\nGolden Rebirth: " .. getGoldRebirth() .. "\nRebirth: " .. getRebirth() .. "\NXP: " .. getXP())
+end
